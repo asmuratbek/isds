43f47454396e4788e9894ff0c064e4c49945baea
muratbek <asmuratbek@gmail.com>
2017-07-13 20:08:04 +0600

    socila is edited

--- social/admin.py
+++ social/admin.py
@@ -5,7 +5,7 @@ from .models import *
 # Register your models here.
 
 class SocialLinksAdmin(admin.ModelAdmin):
-    list_display = ('title', 'f_link', 't_link', 'i_link', 'y_link')
-    fields = ['f_link', 't_link', 'i_link', 'y_link']
+    list_display = ('title', 'f_link', 'y_link',)
+    fields = ['f_link', 'y_link']
 
 admin.site.register(SocialLinks, SocialLinksAdmin)
--- social/models.py
+++ social/models.py
@@ -9,8 +9,6 @@ from django.db import models
 class SocialLinks(models.Model):
     title = models.CharField(max_length=300, default='Редактировать социальные сети')
     f_link = models.CharField(max_length=300, verbose_name='facebook')
-    t_link = models.CharField(max_length=300, verbose_name='twitter')
-    i_link = models.CharField(max_length=300, verbose_name='instagramm')
     y_link = models.CharField(max_length=300, verbose_name='youtube')
 
     class Meta():
